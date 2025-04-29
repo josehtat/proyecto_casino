c87a23d1f590eb9c8dda6a9d9b0c2121fb61a89d
josehtat <jhurtadot33@gmail.com>
2025-04-29 19:19:21 +0200

    pequeños avances con el slot

--- src/scenes/Slot.js
+++ src/scenes/Slot.js
@@ -10,4 +10,23 @@ export class Slot extends Phaser.Scene {
         this.roomCode = data.roomCode; // Recibir el código de la sala
         this.table = data.table; // Recibir la mesa desde la escena principal
     }
+
+    create () {
+        this.socket =  this.game.socket;
+        this.session = this.game.session;
+
+        this.tablePlayers = [];
+
+        console.log('Escena de Slot creada');
+        this.events.emit('blockPlayerMovement', true);
+        // Crear el fondo de la mesa
+        // no hay imagen de fondo, solo un rectángulo translucido gris
+        this.add.rectangle(400, 150, 750, 300, 0x000000, 0.5);
+        this.add.text(400, 20, `Mesa de Slot: ${this.table}`, { fontSize: '18px', color: '#fff' }).setOrigin(0.5);
+        // Cerrar esta escena con Esc y devolver el movimiento
+        this.input.keyboard.on('keydown-ESC', () => {
+            this.closeScene();
+        });
+
+    }
 }
