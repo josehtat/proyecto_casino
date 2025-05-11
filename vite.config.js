1942e6ff25ac371a9950a5ae11e58b3e51d7dac5
jpachonguerra <javier.pachon.guerra@gmail.com>
2025-05-11 13:02:08 +0200

    configuración para puesta a punto en producción

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ export default defineConfig({
   root: 'src',  // Indicar la carpeta con tu código fuente
   base: '/',
   build: {
-    outDir: '../dist',  // Directorio donde se generarán los archivos compilados
+    rollupOptions: {
+      input: {
+          main: '/home/super/AWS2/sintesi/proyecto_casino/src/index.html', // Ruta completa al index.html
+          game: '/home/super/AWS2/sintesi/proyecto_casino/src/game.html'   // Ruta completa al game.html
+      }
+  },
+    outDir: '../public',  // Directorio donde se generarán los archivos compilados
   },
   server: {
     hmr: {
